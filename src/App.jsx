6b6cbd281197bfd8d0fbc09814f6801ca8849bf8
Fredrik Carlsson <me@fc.io>
2023-02-01 14:09:59 +0100

    add chatGPT solution to assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,51 @@
 import React, { useState } from 'react'
 import beach from './assets/beach.jpeg'
-import './App.css'
+// import './App.css'
 
-function getActiveClassName (activeArticle, currentArticle) {
-  if (activeArticle ===  currentArticle) {
-    return 'show-article'
-  } else {
-    return 'hide-article'
-  }
-}
+const Article = ({ title, content, picture }) => (
+  <div>
+    <h3>{title}</h3>
+    <img src={picture} alt="" />
+    <p>{content}</p>
+  </div>
+);
 
-function App() {
-  const [activeArticle, setActiveArticle] = useState('First Article')
+const ArticleList = ({ articles, page, setPage }) => {
+  const startIndex = (page - 1);
+  const endIndex = startIndex + 1;
+  const visibleArticles = articles.slice(startIndex, endIndex);
 
   return (
-    <>
-      <article className={`active ${getActiveClassName(activeArticle, 'First Article')}`} >
-        <h1>About Me</h1>
-        <p>Fredrik är en frilansande front-end utvecklare med över 10 års erfarenhet av JavaScript och JavaScript-ramverk. Han har tidigare jobbat med bland annat Apple, Google och Sony. Fredrik har tidigare föreläst om webb-programmering för högskolestudenter och hållit i presentationer för olika user groups. Fredrik älskar att testa nya ramverk och bibliotek men har sedan lång tid tillbaka haft React som favorit.</p>
-        <img className='beach' src={beach} alt="" />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque nunc, lacinia non sollicitudin ut, congue lobortis nibh. Vivamus interdum elit ac consequat luctus. Etiam a turpis in turpis sagittis dignissim ornare ut tellus. Mauris et accumsan elit. Fusce scelerisque leo a erat tristique, in ornare mauris mollis. Donec hendrerit turpis eleifend vulputate eleifend. Maecenas pretium quis purus vitae luctus.</p>
-      </article>
-      <article className={`active ${getActiveClassName(activeArticle, '2nd Article')}`} >
-        <h1>Idea</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque nunc, lacinia non sollicitudin ut, congue lobortis nibh. Vivamus interdum elit ac consequat luctus. Etiam a turpis in turpis sagittis dignissim ornare ut tellus. Mauris et accumsan elit. Fusce scelerisque leo a erat tristique, in ornare mauris mollis. Donec hendrerit turpis eleifend vulputate eleifend. Maecenas pretium quis purus vitae luctus.</p>
-        <img className='beach' src={beach} alt="" />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque nunc, lacinia non sollicitudin ut, congue lobortis nibh. Vivamus interdum elit ac consequat luctus. Etiam a turpis in turpis sagittis dignissim ornare ut tellus. Mauris et accumsan elit. Fusce scelerisque leo a erat tristique, in ornare mauris mollis. Donec hendrerit turpis eleifend vulputate eleifend. Maecenas pretium quis purus vitae luctus.</p>
-      </article>
-      <div className='article'>
-        <button onClick={() => {
-          setActiveArticle((a) => {
-            if (a === 'First Article') {
-              return '2nd Article'
-            } else {
-              return 'First Article'
-            }
-          })
-        }}>Switch Article</button>
+    <div>
+      {visibleArticles.map(({title, content, picture}) => (
+        <Article key={title} title={title} content={content} picture={picture} />
+      ))}
+      <div>
+        {page > 1 && (
+          <button onClick={() => setPage(page - 1)}>Previous</button>
+        )}
+        {articles.length > endIndex && (
+          <button onClick={() => setPage(page + 1)}>Next</button>
+        )}
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+const ArticlePage = () => {
+  const [page, setPage] = useState(1);
+  const articles = [
+    { title: 'Article 1', content: 'Content 1. adsf asdf as. ssss', picture: beach},
+    { title: 'Article 2', content: 'Content 2' },
+    { title: 'Article 3', content: 'Content 3' },
+    { title: 'Article 4', content: 'Content 4' },
+    { title: 'Article 5', content: 'Content 5' },
+    { title: 'Article 6', content: 'Content 6' },
+  ];
+
+  return (
+    <ArticleList articles={articles} page={page} setPage={setPage} />
+  );
+};
+
+export default ArticlePage;
